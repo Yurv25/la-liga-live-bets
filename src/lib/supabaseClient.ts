59e036a5b0d74466daec57b0e88a3d09fa3bc5bb
Yurisinho <5383897+Yurv25@users.noreply.github.com>
2026-05-05 21:31:05 +0300

    migrate to own supabase, update gitignore

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -27,4 +27,10 @@ export const supabase = createClient(url, anonKey, {
     persistSession: true,
     autoRefreshToken: true,
   },
+  global: {
+    headers: {
+      apikey: anonKey,
+      Authorization: `Bearer ${anonKey}`,
+    },
+  },
 });
